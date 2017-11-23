cd77157cdef1eaa86ce29dd560383d084e6809c7
sonbit <32852496+sonbit@users.noreply.github.com>
2017-11-23 16:19:50 +0100

    fiks js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function openSlideMenu(){
-            document.getElementById('side-menu').style.width = '12%';
-            document.getElementById('wrapper').style.marginLeft = '10%';
+            document.getElementById('side-menu').style.width = '10%';
+            document.getElementById('/wrapper').style.marginLeft = '10%';
         }
         
 function closeSlideMenu(){
             document.getElementById('side-menu').style.width = '0';
-            document.getElementById('wrapper').style.marginLeft = '0';
+            document.getElementById('/wrapper').style.marginLeft = '0';
         }
 
 

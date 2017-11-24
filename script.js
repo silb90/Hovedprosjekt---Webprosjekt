f531bbdd8220571f92cef8fdd22dd0548443c2e6
Jaso Cyril <s325909@stud.hioa.no>
2017-11-24 12:53:54 +0100

    js fix

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function openSlideMenu(){
-            document.getElementById('side-menu').style.width = '14%';
-            document.getElementById('/wrapper').style.marginLeft = '10%';
+            document.getElementById('side-menu').style.width = '12%';
+            document.getElementById('wrapper').style.marginLeft = '10%';
         }
         
 function closeSlideMenu(){
             document.getElementById('side-menu').style.width = '0';
-            document.getElementById('/wrapper').style.marginLeft = '0';
+            document.getElementById('wrapper').style.marginLeft = '0';
         }
 
 

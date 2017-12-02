fc1ce52e8a28b5136dbaf01f3caadebb3ee7cf0b
sonbit <32852496+sonbit@users.noreply.github.com>
2017-12-02 16:46:50 +0100

    Alternativ uten js nav for features + toppknapp

--- script.js
+++ script.js
@@ -3,51 +3,6 @@ function toggleSidebar(ref) {
   document.getElementById('sidebar').classList.toggle('active');
 }
 
-// Start of navbar
-
-scrollTo = (element) => {
-  window.scroll(
-      {
-        behavior: 'smooth',
-        left: 0,
-        top: element.getBoundingClientRect().top + window.scrollY
-      }
-  );
-  console
-}
-
-document.getElementById("1").addEventListener('click', () => {
-  scrollTo(document.getElementById("profile"));
-});
-
-document.getElementById("2").addEventListener('click', () => {
-  scrollTo(document.getElementById("searching"));
-});
-
-document.getElementById("3").addEventListener('click', () => {
-  scrollTo(document.getElementById("chatting"));
-});
-
-document.getElementById("4").addEventListener('click', () => {
-  scrollTo(document.getElementById("communities"));
-});
-
-document.getElementById("5").addEventListener('click', () => {
-  scrollTo(document.getElementById("showcase"));
-});
-
-
-document.getElementById("top").addEventListener('click', () => {
-  window.scroll(0,0);
-});
-
-
-
-
-// End of navbar
-
-
-
 function appSettings() {
     
     // Topbar colors //

dc9b4fd6c088c8b0769c861d15707a43fcf51186
Jaso Cyril <s325909@stud.hioa.no>
2017-12-02 13:50:09 +0100

    fix

--- script.js
+++ script.js
@@ -19,10 +19,6 @@ scrollTo = (element) => {
 document.getElementById("1").addEventListener('click', () => {
   scrollTo(document.getElementById("profile"));
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> 4aabf6f8f3db83802befc416578eaa73152076b3
 
 document.getElementById("2").addEventListener('click', () => {
   scrollTo(document.getElementById("searching"));
